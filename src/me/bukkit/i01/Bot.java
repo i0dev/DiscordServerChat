17788895be04da6a18ae294d256a0a0ad32e9374
i01 <67590686+i0dev@users.noreply.github.com>
2020-11-01 14:36:47 -0600

    Discord API & MC Heads
    Updated the discord JDA builder. and added a MCHead to the embed sent

--- src/me/bukkit/i01/Bot.java
+++ src/me/bukkit/i01/Bot.java
@@ -11,10 +11,11 @@ public class Bot {
 
     public static JDA jda;
 
-    public static void sendServerChat(String message) {
+    public static void sendServerChat(String message, String UUID) {
         EmbedBuilder EmbedServerChat = new EmbedBuilder()
                 .setTimestamp(ZonedDateTime.now())
                 .setFooter(Main.Message_Embed_Footer)
+                .setThumbnail("https://crafatar.com/renders/head/" + UUID)
                 .setColor(Color.decode(Main.ColorHexCode))
                 .setDescription("\n" + message);
 
@@ -23,7 +24,7 @@ public class Bot {
 
     public static void init(String Token) {
         try {
-            jda = new JDABuilder(Token).build();
+            jda = JDABuilder.createDefault(Token).build();
         } catch (LoginException e) {
             e.printStackTrace();
         }
